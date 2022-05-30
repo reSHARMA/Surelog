29904ffb9856ad8a825ca650ba8195dcb80bfb7a
reSHARMA <reshabhsh@gmail.com>
2022-05-29 22:37:17 -0700

    Add test for assign + always_comb

---
+++ cs_tests/5-always-comb/5-always-comb-true-2.sv
@@ -0,0 +1,41 @@
+module test1
+(
+    input logic a, b,
+    output logic y, z
+);
+
+assign a = b;
+
+always_comb begin
+    if (a < b) begin
+        y = 0;
+        z = 1;
+    end
+    else begin
+        y = 1;
+        z = 1;
+    end
+end
+
+endmodule
+
+module test
+(
+    input logic a, b,
+    output logic y, z
+);
+
+assign a = b;
+
+always_comb begin
+    if (a < b) begin
+        y = 0;
+        z = 1;
+    end
+    else begin
+        y = 1;
+        z = 1;
+    end
+end
+
+endmodule
